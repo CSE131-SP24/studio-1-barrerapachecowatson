53dce8dc14adef51a9850ceb5575117fc7ab9b05
watso <watso@Stanley.dhcp.wustl.edu>
2024-01-24 15:56:56 -0600

    Working

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,13 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean is0rdered=true;
+		is0rdered= (x>y)&&(y>z) ;
+		System.out.print(is0rdered);
+		is0rdered = false;
+		is0rdered = (z>y)&&(y>x);
+		
+		
 
 	}
 
